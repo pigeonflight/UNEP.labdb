ba924e9512d32c6e22f4c6ec8010c572569471be
David Bain <david@alteroo.com>
2014-11-19 11:55:47 +0000

    updated manifest and setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = '1.0.2a'
 setup(name='UNEP.labdb',
       version=version,
       description="UNEP Lab DB",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read() ,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
